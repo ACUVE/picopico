391487012b08adf57c2dd2671fdc03bb6a509429
ACUVE <sub_chon@yahoo.co.jp>
2023-10-31 00:41:46 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,252 +1,123 @@
-//! This example shows powerful PIO module in the RP2040 chip.
-
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use core::mem::MaybeUninit;
-
 use embassy_executor::Spawner;
-use embassy_rp::gpio::{Level, Output};
+use embassy_rp::gpio::{AnyPin, Input, Level, Output, Pin, Pull};
+use embassy_rp::uart::{self, Uart};
 use embassy_rp::{bind_interrupts, config};
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::signal::Signal;
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
-    PIO0_IRQ_0 => embassy_rp::pio::InterruptHandler<embassy_rp::peripherals::PIO0>;
-    USBCTRL_IRQ => embassy_rp::usb::InterruptHandler<embassy_rp::peripherals::USB>;
+    UART0_IRQ => embassy_rp::uart::InterruptHandler<embassy_rp::peripherals::UART0>;
 });
 
-mod codes {
-    // Audio Interface Class Code
-    pub const AUDIO: u8 = 0x01;
+// ループコイルの入力を受け付ける（PullUp で High が非検知、 Low が検知）
+// その入力を、精算機に UART で伝送する
+// 精算機からシリアル通信を受け、サーボモータを制御する
 
-    // Audio Interface Subclass Codes
-    pub const SUBCLASS_UNDEFINED: u8 = 0x00;
-    pub const AUDIOCONTROL: u8 = 0x01;
-    pub const AUDIOSTREAMING: u8 = 0x02;
-    pub const MIDISTREAMING: u8 = 0x03;
+async fn uart_task(
+    uart: Uart<'_, impl uart::Instance, uart::Async>,
+    coil: &Signal<impl embassy_sync::blocking_mutex::raw::RawMutex, bool>,
+    servo: &Signal<impl embassy_sync::blocking_mutex::raw::RawMutex, bool>,
+) {
+    let (mut tx, mut rx) = uart.split();
 
-    // Audio Interface Protocol Codes
-    pub const PR_PROTOCOL_UNDEFINED: u8 = 0x00;
+    embassy_futures::join::join(
+        async {
+            servo.signal(false);
 
-    // Audio Class-Specific Descriptor Types
-    pub const CS_UNDEFINED: u8 = 0x20;
-    pub const CS_DEVICE: u8 = 0x21;
-    pub const CS_CONFIGURATION: u8 = 0x22;
-    pub const CS_STRING: u8 = 0x23;
-    pub const CS_INTERFACE: u8 = 0x24;
-    pub const CS_ENDPOINT: u8 = 0x25;
-
-    // Audio Class-Specific AC Interface Descriptor Subtypes
-    pub const AC_DESCRIPTOR_UNDEFINED: u8 = 0x00;
-    pub const HEADER: u8 = 0x01;
-    pub const INPUT_TERMINAL: u8 = 0x02;
-    pub const OUTPUT_TERMINAL: u8 = 0x03;
-    pub const MIXER_UNIT: u8 = 0x04;
-    pub const SELECTOR_UNIT: u8 = 0x05;
-    pub const FEATURE_UNIT: u8 = 0x06;
-    pub const PROCESSING_UNIT: u8 = 0x07;
-    pub const EXTENSION_UNIT: u8 = 0x08;
-
-    // Audio Class-Specific AS Interface Descriptor Subtypes
-    pub const AS_DESCRIPTOR_UNDEFINED: u8 = 0x00;
-    pub const AS_GENERAL: u8 = 0x01;
-    pub const FORMAT_TYPE: u8 = 0x02;
-    pub const FORMAT_SPECIFIC: u8 = 0x03;
-
-    // Processing Unit Process Types
-    pub const PROCESS_UNDEFINED: u8 = 0x00;
-    pub const UP_DOWNMIX_PROCESS: u8 = 0x01;
-    pub const DOLBY_PROLOGIC_PROCESS: u8 = 0x02;
-    pub const THREED_STEREO_EXTENDER_PROCESS: u8 = 0x03;
-    pub const REVERBERATION_PROCESS: u8 = 0x04;
-    pub const CHORUS_PROCESS: u8 = 0x05;
-    pub const DYN_RANGE_COMP_PROCESS: u8 = 0x06;
-
-    // DESCRIPTOR_UNDEFINED
-    pub const DESCRIPTOR_UNDEFINED: u8 = 0x00;
-    pub const EP_GENERAL: u8 = 0x01;
-
-    // Audio Class-Specific Request Codes
-    pub const REQUEST_CODE_UNDEFINED: u8 = 0x00;
-    pub const SET_CUR: u8 = 0x01;
-    pub const GET_CUR: u8 = 0x81;
-    pub const SET_MIN: u8 = 0x02;
-    pub const GET_MIN: u8 = 0x82;
-    pub const SET_MAX: u8 = 0x03;
-    pub const GET_MAX: u8 = 0x83;
-    pub const SET_RES: u8 = 0x04;
-    pub const GET_RES: u8 = 0x84;
-    pub const SET_MEM: u8 = 0x05;
-    pub const GET_MEM: u8 = 0x85;
-    pub const GET_STAT: u8 = 0xFF;
+            let mut buff = [0; 1];
+            loop {
+                use embassy_rp::uart::Error::*;
+                match rx.read(&mut buff).await {
+                    Ok(()) => {
+                        // TODO: なんか通信処理する
+                        servo.signal(true);
+                    }
+                    Err(Overrun) => {}
+                    Err(Break) => {}
+                    Err(Parity) => {}
+                    Err(Framing) => {}
+                    Err(_) => {}
+                }
+            }
+        },
+        async {
+            loop {
+                let current = coil.wait().await;
+                use embassy_rp::uart::Error::*;
+                // TODO: なんか通信処理する
+                match tx.write(&[current as u8]).await {
+                    Ok(()) => {}
+                    Err(Overrun) => {}
+                    Err(Break) => {}
+                    Err(Parity) => {}
+                    Err(Framing) => {}
+                    Err(_) => {}
+                }
+            }
+        },
+    )
+    .await;
 }
 
-use codes::*;
-use embassy_usb_driver::{Endpoint, EndpointIn};
-
-struct Handler {}
-
-impl embassy_usb::Handler for Handler {
-    fn control_in<'a>(
-        &'a mut self,
-        _req: embassy_usb::control::Request,
-        _buf: &'a mut [u8],
-    ) -> Option<embassy_usb::control::InResponse<'a>> {
-        None
-    }
-    fn control_out(
-        &mut self,
-        _req: embassy_usb::control::Request,
-        _data: &[u8],
-    ) -> Option<embassy_usb::control::OutResponse> {
-        None
+async fn coil_task(
+    mut input: Input<'_, AnyPin>,
+    coil: &Signal<impl embassy_sync::blocking_mutex::raw::RawMutex, bool>,
+) {
+    let mut current_level: bool = input.get_level().into();
+    loop {
+        coil.signal(!current_level);
+        if current_level {
+            input.wait_for_low().await;
+            current_level = false;
+        } else {
+            input.wait_for_high().await;
+            current_level = true;
+        }
     }
 }
 
-// In は Host から見て Device が In するので、Device は Write する
+async fn servo_task(
+    servo: &Signal<impl embassy_sync::blocking_mutex::raw::RawMutex, bool>,
+    mut output: Output<'_, AnyPin>,
+) {
+    loop {
+        // サーボモータを制御する
+        let current = servo.wait().await;
+
+        // TODO: なんかサーボモータを制御する
+        output.set_level(current.into());
+    }
+}
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let config = config::Config::default();
     let p = embassy_rp::init(config);
 
-    let _g = Output::new(p.PIN_16, Level::High);
-    let mut r = Output::new(p.PIN_17, Level::High);
-    let mut b = Output::new(p.PIN_25, Level::High);
-
-    let usb = p.USB;
-    let driver = embassy_rp::usb::Driver::new(usb, Irqs);
-
-    let mut usb_config = embassy_usb::Config::new(0xee, 0xff);
-    usb_config.serial_number = Some("THISISTESTERIALNUMBER");
-
-    let mut device_descriptor_buf = [0u8; 128];
-    let mut config_descriptor_buf = [0u8; 512];
-    let mut bos_descriptor_buf = [0u8; 128];
-    let mut control_buf = [0u8; 128];
-
-    // let mut handler = Handler {};
-    let mut handler = MaybeUninit::uninit();
-
-    let mut builder = embassy_usb::Builder::new(
-        driver,
-        usb_config,
-        &mut device_descriptor_buf,
-        &mut config_descriptor_buf,
-        &mut bos_descriptor_buf,
-        &mut control_buf,
+    let input = Input::new(p.PIN_26.degrade(), Pull::Up);
+    let output = Output::new(p.PIN_27.degrade(), Level::High);
+    let uart = Uart::new(
+        p.UART0,
+        p.PIN_0,
+        p.PIN_1,
+        Irqs,
+        p.DMA_CH0,
+        p.DMA_CH1,
+        Default::default(),
     );
 
-    // device -> config(function) -> interface -> alternate setting -> endpoint
-    // 通常、 1 interface が 1 driver に対応するが、IAD を使うと 1 driver が複数の interface を持てるので、統合して、 function と呼ぶ
-    // そもそも configulation には class はない（IAD でないと、この function に与えた class などは無視される）
-    let mut function = builder.function(0x00, 0x00, 0x00);
-    let mut interface = function.interface();
-    let mut alt_setting = interface.alt_setting(AUDIO, AUDIOCONTROL, PR_PROTOCOL_UNDEFINED, None);
-    alt_setting.descriptor(
-        CS_INTERFACE,
-        &[
-            HEADER, // bDescriptorSubtype
-            0x00, 0x01, // bcdADC
-            0x1e, 0x00, // wTotalLength
-            0x01, // bInCollection
-            0x01, // baInterfaceNr
-        ],
-    );
-    alt_setting.descriptor(
-        CS_INTERFACE,
-        &[
-            INPUT_TERMINAL, // bDescriptorSubtype
-            0x01,           // bTerminalID
-            0x01,
-            0x02, // wTerminalType
-            0x00, // bAssocTerminal
-            0x01, // bNrChannels
-            0x00,
-            0x00, // wChannelConfig
-            0x00, // iChannelNames
-            0x00, // iTerminal
-        ],
-    );
-    alt_setting.descriptor(
-        CS_INTERFACE,
-        &[
-            OUTPUT_TERMINAL, // bDescriptorSubtype
-            0x02,            // bTerminalID
-            0x01,
-            0x01, // wTerminalType
-            0x00, // bAssocTerminal
-            0x01, // bSourceID
-            0x00, // iTerminal
-        ],
-    );
-    let mut interface = function.interface();
-    let mut alt_setting = interface.alt_setting(AUDIO, AUDIOSTREAMING, PR_PROTOCOL_UNDEFINED, None);
-    let mut alt_setting = interface.alt_setting(AUDIO, AUDIOSTREAMING, PR_PROTOCOL_UNDEFINED, None);
-    alt_setting.descriptor(
-        CS_INTERFACE,
-        &[
-            AS_GENERAL, // bDescriptorSubtype
-            0x02,       // bTerminalLink
-            0x01,       // bDelay
-            0x01, 0x00, // wFormatTag
-        ],
-    );
-    alt_setting.descriptor(
-        CS_INTERFACE,
-        &[
-            FORMAT_TYPE, // bDescriptorSubtype
-            0x01,        // bFormatType
-            0x01,        // bNrChannels
-            0x02,        // bSubframeSize
-            0x10,        // bBitResolution
-            0x01,        // bSamFreqType
-            0x40,
-            0x1f,
-            0x00, // tSamFreq
-        ],
-    );
-    let mut endpoint = alt_setting.endpoint_isochronous_in(0x10, 0x01);
-    alt_setting.descriptor(
-        CS_ENDPOINT,
-        &[
-            EP_GENERAL, 0x00, // bmAttributes
-            0x00, // bLockDelayUnits
-            0x00, 0x00, // wLockDelay
-        ],
-    );
+    let coil_signal = Signal::<CriticalSectionRawMutex, _>::new();
+    let servo_signal = Signal::<CriticalSectionRawMutex, _>::new();
 
-    let handler = handler.write(Handler {});
-    drop(function);
-
-    builder.handler(handler);
-
-    let mut device = builder.build();
-
-    b.set_low();
-
-    const SQUARE: [u8; 16] = [
-        0xFF, 0x7F, 0xFF, 0x7F, 0xFF, 0x7F, 0xFF, 0x7F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00,
-    ];
-
-    embassy_futures::join::join(device.run(), async {
-        loop {
-            endpoint.wait_enabled().await;
-            // write 16 bytes 0x00
-            loop {
-                r.set_level(r.is_set_low().into());
-                match endpoint.write(&SQUARE).await {
-                    Ok(()) => {}
-                    Err(embassy_usb_driver::EndpointError::BufferOverflow) => {
-                        continue;
-                    }
-                    Err(embassy_usb_driver::EndpointError::Disabled) => {
-                        break;
-                    }
-                }
-            }
-        }
-    })
+    embassy_futures::join::join3(
+        uart_task(uart, &coil_signal, &servo_signal),
+        servo_task(&servo_signal, output),
+        coil_task(input, &coil_signal),
+    )
     .await;
 }
